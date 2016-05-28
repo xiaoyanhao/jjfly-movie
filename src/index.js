21402d274f6bc4917850d38f3829357039865e52
xiaoyanhao <270411549@qq.com>
2016-05-28 19:10:57 +0800

    init env

---
+++ src/index.js
@@ -0,0 +1,16 @@
+class JJFly extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <p>Movie</p>
+    )
+  }
+}
+
+ReactDOM.render(
+  <JJFly />,
+  document.body
+)
